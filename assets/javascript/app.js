3e7f53e21f391da2b2f66cc19bc8c2ad6aa2094d
Randamus <rcviets@gmail.com>
2019-09-21 22:23:16 -0600

    original topic buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,31 @@
+$(document).ready(function(){
+// Topics Array
 var topics = ['mercury', 'venus', 'mars', 'jupiter', 'saturn', 'neptune', 'uranus', 'pluto', 'nasa', 'spacex'];
 
+origButtons();
+
+// Topic Buttons
+function origButtons() {
+    for(var i = 0; i < topics.length; i++) {
+        var origDiv = $('<div>');
+        var origButtons = $("<button>");
+        origButtons.addClass('origButtons');
+        origButtons.attr('data-name', topics[i]);
+        origButtons.text(topics[i]);
+        
+        origDiv.append(origButtons);
+        $("#buttons-div").append(origButtons);
+    }
+}
+
+// Event Listener for all Buttons
+
+// AJAX GET Method
+
+// Filter by Rating
+
+// Add to Gif Div
+
+// Toggle Pause on Gifs
+
+});
