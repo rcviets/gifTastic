f023bea33ac856fe71d524c457bfd2ad1393ea4a
Randamus <rcviets@gmail.com>
2019-09-21 22:59:08 -0600

    append gifs to div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,20 +20,33 @@ $(document).ready(function () {
     // Event Listener for all Buttons
     $('.topicButtons').click(function () {
         var thisTopic = $(this).attr('data-name');
-        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
-            thisTopic + "&api_key=LXwFIEjBvFNKbrVWpPlCpIIj6rIB1A7A&limit=10&offset=0&rating=G&lang=en";
-    // AJAX GET Method
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    })
-        // Once Data is returned
-        .then(function(response) {
-            console.log(response)
-            console.log(this)
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=LXwFIEjBvFNKbrVWpPlCpIIj6rIB1A7A&q=" + 
+        thisTopic + "&limit=10&offset=0&rating=G&lang=en";
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
         })
+            // Once Data is returned
+            .then(function (response) {
+                console.log(response)
+                console.log(this)
+
+                var results = response.data;
+
+                // Add to Gif Div
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $('<div>')
+                    var p = $('<p>').text("Rating: " + results[i].rating);
+                    var gifImage = $('<img>');
+                    gifImage.attr('src', results[i].images.fixed_height.url);
+
+                    gifDiv.append(gifImage);
+                    gifDiv.append(p);
+                    $('#gifs-div').prepend(gifDiv);
+                }
+            })
     });
-    // Add to Gif Div
 
     // Toggle Pause on Gifs
 
