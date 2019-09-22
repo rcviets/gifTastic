b5e7758ee35fb9c4acf4c514e5edc687f90a9108
Randamus <rcviets@gmail.com>
2019-09-22 00:34:23 -0600

    add new topic button push/no click event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
     // Topics Array
     var topics = ['mercury', 'venus', 'mars', 'jupiter', 'saturn', 'neptune', 'uranus', 'pluto', 'nasa', 'spacex'];
 
-    origButtons();
+    topicButtons();
 
     // Topic Buttons
-    function origButtons() {
+    function topicButtons() {
+        $("#buttons-div").empty();
         for (var i = 0; i < topics.length; i++) {
             var topicDiv = $('<div>');
             var topicButtons = $("<button>");
@@ -20,9 +21,10 @@ $(document).ready(function () {
     // Event Listener for all Buttons
     $('.topicButtons').click(function () {
         $('#gifs-div').empty();
+
         var thisTopic = $(this).attr('data-name');
-        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=LXwFIEjBvFNKbrVWpPlCpIIj6rIB1A7A&q=" + 
-        thisTopic + "&limit=10&offset=0&rating=G&lang=en";
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=LXwFIEjBvFNKbrVWpPlCpIIj6rIB1A7A&q=" +
+            thisTopic + "&limit=10&offset=0&rating=G&lang=en";
 
         $.ajax({
             url: queryUrl,
@@ -41,12 +43,22 @@ $(document).ready(function () {
                     var p = $('<p>').text("Rating: " + results[i].rating);
                     var gifImage = $('<img>');
                     gifImage.attr('src', results[i].images.fixed_height.url);
-                    
+
                     gifDiv.append(gifImage);
                     gifDiv.append(p);
                     $('#gifs-div').prepend(gifDiv);
                 }
             })
+
+    });
+    // Add a New Gif Topic
+    $("#add-topic").click(function (event) {
+        event.preventDefault();
+        var newTopic = $("#topic-input").val().trim();
+
+        topics.push(newTopic);
+        topicButtons();
+
     });
 
     // Toggle Pause on Gifs
